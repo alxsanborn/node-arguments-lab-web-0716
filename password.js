ba5500a2cbfbc4d6f46886b594fffbd2a0ed7d25
jasmin47 <alxsanborn@gmail.com>
2016-10-28 09:20:27 -0400

    created passwords according to length

--- password.js
+++ password.js
@@ -1,1 +1,10 @@
-// code solution here
+var password = ""
+ process.argv.shift()
+ process.argv.shift()
+
+ process.argv.forEach(function pass(length, number){
+   for (var i = 0; i < length; i++){
+   password += "n"
+ }
+ console.log(password)
+ })
